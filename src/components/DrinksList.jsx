3f17bcc066ea624db41497bde3c0c4431ba8bd89
Josue Gallegos <gallegosjosue6@gmail.com>
2023-10-11 18:19:11 -0500

    added table row to DrinksList.jsx

--- src/components/DrinksList.jsx
+++ src/components/DrinksList.jsx
@@ -1,6 +1,8 @@
 //DrinksList
 
 import { useEffect, useState } from "react";
+import DrinksTableRow from "./DrinksTableRow";
+
 
 const DrinksList = () => {
 
@@ -25,19 +27,12 @@ const DrinksList = () => {
 
 
     const renderedDrinks = drinks.map((drink) => (
-        <tr key={drink.strDrink}>
-        <td>{drink.strDrink}</td>
-        <td>
-            <img 
-                src={drink.strDrinkThumb} 
-                alt={drink.strDrink} 
-                style={{width: "100px", height: "100px"}}
-
-            />
-        </td>
-
-        </tr>
-    ))
+        <DrinksTableRow 
+        drink={drink}
+        key={drink.strDrink} 
+            
+        />
+    ));
 
     return (
         <>
